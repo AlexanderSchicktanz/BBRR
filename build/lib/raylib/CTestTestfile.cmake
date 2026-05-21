415061ac862b6fed64214e4ed2093dda1ebdbde8
alex <alexander.schicktanz@web.de>
2026-05-22 01:25:21 +0200

    Removed unwanted files marked in .gitignore

--- build/lib/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/alex/Dokumente/Vault/Semester_1/GDS/HiWi/RISC-V/BBRR/lib/raylib
-# Build directory: /home/alex/Dokumente/Vault/Semester_1/GDS/HiWi/RISC-V/BBRR/build/lib/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("raylib")
--- build/libriscv/emulator/CMakeFiles/rvlinux.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for rvlinux.
-# This may be replaced when dependencies are built.
